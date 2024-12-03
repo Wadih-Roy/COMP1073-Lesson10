7873e85553bac358ee45fb0a2a1ebd6f833cde2e
Wadih-Roy <wadihroy01@gmail.com>
2024-12-03 10:34:56 -0500

    fixed json and image links

--- lesson-10/js/json.js
+++ lesson-10/js/json.js
@@ -6,7 +6,7 @@ const section = document.querySelector("section");
 async function populate() {
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    const requestUrl = "https://ryanpatton8.github.io/COMP1073-Lab04/week_10/js/i-scream.json";
+    const requestUrl = "https://nibraskhalid.github.io/COMP1073Week10/js/i-scream.json";
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(requestUrl);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
@@ -47,7 +47,7 @@ function showTopFlavors(jsonObj) {
 
         // STEP 10f: Set the textContent property for each of the above elements (except the UL), based on the JSON content
         h2.textContent = topFlavors[i]['name'];
-        image.setAttribute("src", `https://ryanpatton8.github.io/COMP1073-Lab04/week_10/images/${topFlavors[i]["image"]}`);
+        image.setAttribute("src", `https://nibraskhalid.github.io/COMP1073Week10/images/${topFlavors[i]["image"]}`);
             
         // STEP 10g: Build a loop for the ingredients array in the JSON
         ingredients = topFlavors[i]["ingredients"];
